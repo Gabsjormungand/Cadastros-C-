d11ca78f0c3d14402f722a60d013ec92af1245e8
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-05-22 22:22:21 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastros C#/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Cadastros_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.col_ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Produto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Quantidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Preco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.txtBoxProduto = new System.Windows.Forms.TextBox();
+            this.txtBoxQuali = new System.Windows.Forms.TextBox();
+            this.txtBoxPreco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnObter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_ID,
+            this.col_Produto,
+            this.col_Quantidade,
+            this.col_Preco});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(46, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(424, 233);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // col_ID
+            // 
+            this.col_ID.Text = "ID";
+            this.col_ID.Width = 40;
+            // 
+            // col_Produto
+            // 
+            this.col_Produto.Text = "Produto";
+            this.col_Produto.Width = 200;
+            // 
+            // col_Quantidade
+            // 
+            this.col_Quantidade.Text = "Quantidade";
+            this.col_Quantidade.Width = 100;
+            // 
+            // col_Preco
+            // 
+            this.col_Preco.Text = "Preço";
+            this.col_Preco.Width = 80;
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Location = new System.Drawing.Point(45, 288);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(58, 20);
+            this.txtBoxID.TabIndex = 1;
+            // 
+            // txtBoxProduto
+            // 
+            this.txtBoxProduto.Location = new System.Drawing.Point(109, 288);
+            this.txtBoxProduto.Name = "txtBoxProduto";
+            this.txtBoxProduto.Size = new System.Drawing.Size(126, 20);
+            this.txtBoxProduto.TabIndex = 2;
+            // 
+            // txtBoxQuali
+            // 
+            this.txtBoxQuali.Location = new System.Drawing.Point(254, 288);
+            this.txtBoxQuali.Name = "txtBoxQuali";
+            this.txtBoxQuali.Size = new System.Drawing.Size(105, 20);
+            this.txtBoxQuali.TabIndex = 3;
+            // 
+            // txtBoxPreco
+            // 
+            this.txtBoxPreco.Location = new System.Drawing.Point(375, 288);
+            this.txtBoxPreco.Name = "txtBoxPreco";
+            this.txtBoxPreco.Size = new System.Drawing.Size(105, 20);
+            this.txtBoxPreco.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Produto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Qualidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Preço";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(45, 333);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 29);
+            this.btnAdicionar.TabIndex = 9;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(45, 368);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 29);
+            this.btnRemover.TabIndex = 10;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnObter
+            // 
+            this.btnObter.Location = new System.Drawing.Point(45, 403);
+            this.btnObter.Name = "btnObter";
+            this.btnObter.Size = new System.Drawing.Size(75, 28);
+            this.btnObter.TabIndex = 11;
+            this.btnObter.Text = "Obter";
+            this.btnObter.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 611);
+            this.Controls.Add(this.btnObter);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxPreco);
+            this.Controls.Add(this.txtBoxQuali);
+            this.Controls.Add(this.txtBoxProduto);
+            this.Controls.Add(this.txtBoxID);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader col_ID;
+        private System.Windows.Forms.ColumnHeader col_Produto;
+        private System.Windows.Forms.ColumnHeader col_Quantidade;
+        private System.Windows.Forms.ColumnHeader col_Preco;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.TextBox txtBoxProduto;
+        private System.Windows.Forms.TextBox txtBoxQuali;
+        private System.Windows.Forms.TextBox txtBoxPreco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnObter;
+    }
+}
+
---
+++ Cadastros C#/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastros_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            txtBoxID.Clear();
+            txtBoxProduto.Clear();
+            txtBoxQuali.Clear();
+            txtBoxPreco.Clear();
+            txtBoxID.Focus();
+        }
+
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (txtBoxID.Text == "")
+            {
+                MessageBox.Show("ID Não pode ser vazio, por favor preencha o id");
+                txtBoxID.Focus();
+                return;
+            }
+
+            if (txtBoxProduto.Text == "")
+            {
+                MessageBox.Show("Produto Não pode ser vazio, por favor preencha o Produto");
+                txtBoxProduto.Focus();
+                return;
+            }
+
+            if (txtBoxQuali.Text == "")
+            {
+                MessageBox.Show("Qualidade Não pode ser vazio, por favor preencha o Qualidade");
+                txtBoxQuali.Focus();
+                return;
+            }
+
+            if (txtBoxPreco.Text == "")
+            {
+                MessageBox.Show("PREço Não pode ser vazio, por favor preencha o Preço"):
+                txtBoxPreco.Focus();
+                return;
+            }
+        }
+    }
+}
